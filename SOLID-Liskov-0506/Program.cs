5db8c3a41646cd45525098da0ad189faabbe8d91
Gusts Linkevics <gusts@gst.lv>
2023-06-19 18:57:55 +0300

    Add project files.

---
+++ SOLID-Liskov-0506/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SOLID_PRINCIPLES.LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Orange();
+            Console.WriteLine(apple.GetColor());
+        }
+    }
+    public class Apple
+    {
+        public virtual string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : Apple
+    {
+        public override string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
